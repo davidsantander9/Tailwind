2abb7fe95c76906a82e06a9c700861939da00abb
David <davidsantander9@gmail.com>
2021-08-05 20:52:55 -0500

    custom tailwind

---
+++ project/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        danger: '#ff5f40',
+        info: {
+          100: '#24a19c',
+          200: '#6ebfb5',
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
